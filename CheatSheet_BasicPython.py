9f9194c5ad688f89b060ceef7152431d92d6aca5
georgejake <georgeputhen@yahoo.com>
2023-05-01 02:16:59 -0700

    Added few more cheats

--- CheatSheet_BasicPython.py
+++ CheatSheet_BasicPython.py
@@ -1,5 +1,7 @@
 # Basic variable assignment ,Calculating area of a triangle
 import json
+import calendar;
+import math;
 
 base = 12
 height = 10
@@ -58,6 +60,7 @@ else:
 """
 In PYTHON ,Dictionary closely resembles json. 
 Initialize a JSON, Dump JSON to string,Dump JSON to a file, Read JSON document and parse
+Why relative path using dot natoation is not working
 """
 books = {}
 books["tom"] = {
@@ -74,10 +77,10 @@ print(books)
 s = json.dumps(books)
 print(s)
 # Writing to file
-with open("/MyFiles/HelloJson", "w") as f:
+with open("/home/george81/PycharmProjects/HelloWorld/MyFiles/HelloJson", "w") as f:
     f.write(s)
 # Reading from file
-f = open("/MyFiles/HelloJson", "r")
+f = open("/home/george81/PycharmProjects/HelloWorld/MyFiles/HelloJson", "r")
 s = f.read()
 print(s)
 loadedBook = json.loads(s)
@@ -85,11 +88,11 @@ print(loadedBook['tom'])
 print(loadedBook['tom']['phone'])
 
 for person in books:
-    #This only prints tom & george ,basically the keys
+    # This only prints tom & george ,basically the keys
     print(person)
     print(books[person])
 
-#List in python
+# List in python
 """
 List are fundamental building block of collections in python
 Lists are one of 4 built-in data types in Python used to store collections of data, 
@@ -112,5 +115,59 @@ else:
 
 thislist = list(("apple", "banana", "cherry"))
 print(thislist)
-#Loops
+# Loops
+"""
+How to loop through a list; Break v/s Continue; Range
+"""
+my_exp_list = [10, 20, 30]
+total = 0
+for each_item in my_exp_list:
+    total = total + each_item
+print(total)
+for i in range(1, 11):
+    print(i)
+for i in range(len(my_exp_list)):
+    print(i)
+key_location = "chair"
+locations = ["almirah", "desk", "table", "chair", "bed"]
+for location in locations:
+    if location == key_location:
+        print("key found in ", location)
+        break;
+    else:
+        print("key not found in ", location)
 
+for i in range(1, 5):
+    if i % 2 == 0:
+        print(i * i)
+    else:
+        continue
+# Modules
+"""
+Importing modules. Printing all functions in modules
+Imported calendar & math
+"""
+cal = calendar.month(2023, 4)
+print(cal)
+print(math.sqrt(16))
+print(dir(math))
+# Importing our modules
+"""
+import HelloFunction as hf
+If function exist in some other path we have to add in system path
+sys.path.append("path of function")
+import the function after appending to sys path
+"""
+# Tuples
+"""
+Tuples are used to store multiple items in a single variable.Tuples can have mix of data types
+Tuples are objects with data type as tuple.A tuple is a collection which is ordered and unchangeable(immutable)
+Tuples allow duplicates
+"""
+point = (5, 9)
+print(point[1])
+print(type(point))
+point1 = ("test", "mango", 1, 2)
+print(point1)
+point2 = ("test", "mango", 1, 2, "test", "mango")
+print(point2)
